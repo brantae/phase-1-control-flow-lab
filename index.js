9318483c9bf4201f2ded77e927bc0877ad55b79b
Anslie <brantae@gmail.com>
2022-07-15 18:05:13 -0500

    commit tree 9d2258b9f9e208618d3177b1fab25af5a8bd276e

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  
+  let result;
+  if (rideLength <= 400) {
+    return result = 'This one is on me!';
+  }
+  else if (rideLength > 400 && rideLength < 2000) {
+    return result = 'That will be twenty bucks.';
+  }
+  else if (rideLength > 2000 && rideLength <= 2500) {
+    return result = 'I will gladly take your thirty bucks.';
+  }
+  else if (rideLength > 2500) {
+    return result = 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  let result;
+if (city == 'NYC') {
+  return result = 'Ok, sounds good.';
+}
+  else {
+    return result = 'No go.';
+  }
+
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+
+  let result;
+if (tipAmount == 'generous') {
+  return result = 'Thank you so much.';
+}
+else if (tipAmount == 'not as generous') {
+  return result = 'Thank you.';
+
+}
+else {
+  return result = 'Bye.'
+}
 }
